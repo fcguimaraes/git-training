80e96ca432d48decb13cec7a2ab49ccbc03c43dc
Fernando Guimaraes <fernando3guimaraes@gmail.com>
2021-07-05 16:27:09 -0300

    New small modification

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: 5 de jul de 2021
+ *      Author: abacusraspi
+ */
+#include <stdio.h>
+
+int main(void){
+    printf("\n\nHello world!\n\n");
+
+    return 0;
+}
+
